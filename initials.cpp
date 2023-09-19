d81048f64a134c8ec3679a913532294bd642e082
Reza Partovi <reza.partovi96@gmail.com>
2023-09-18 23:24:20 -0700

    First Commit

---
+++ initials.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+int main() {
+  
+  std::cout << "RRRR " << "  " << "PPPP \n";
+  std::cout << "R   R" << "  " << "P   P\n"; 
+  std::cout << "R   R" << "  " << "P   P\n";
+  std::cout << "RRRR " << "  " << "PPPP \n";
+  std::cout << "R R  " << "  " << "P    \n";
+  std::cout << "R  R " << "  " << "P    \n";
+  std::cout << "R   R" << "  " << "P    \n";
+
+return 0;  
+}
